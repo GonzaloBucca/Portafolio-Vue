2b297006fc29a4a2790c1a261326f5a80238aa77
Gonzalo Bucca <gonzalobucca1@gmail.com>
2024-11-02 18:52:59 -0300

    Agrego Portafolio hasta la clase 9

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/EducacionComponente.vue';
+import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
+import HabilidadesComponente from './components/HabilidadesComponente.vue';
+</script>
+
+<template>
+<header>
+  <NavBar/>
+  <DatosPersonales/>
+</header>
+<main>
+    <section id="educacion"><h2>Educación - Cursos</h2>
+      <Educacion />
+    </section>
+    <section id="experiencia"><h2>Experiencia</h2>
+      <ExperienciaComponente/>
+    </section>
+    <section id="proyectos"><h2>Proyectos</h2>
+      <ProyectosComponente/>
+    </section>
+    <section id="habilidades"><h2>Habilidades</h2>
+    <HabilidadesComponente/>
+    </section>
+    <section id="intereses"><h2>Intereses</h2></section>
+</main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ExperienciaComponente.vue
@@ -0,0 +1,56 @@
+<script setup>
+import icono1 from '/src/assets/programmer-svgrepo-com.svg';
+import { ref } from 'vue';
+const titulo = 'Tecnicatura en Programación';
+const fecha = '2023/24';
+const experiencias = ref([
+    { id: 1, src: icono1, parrafo: '2 año de cursado de la Tecnicatura en Programación.' },
+]);
+</script>
+
+<template>
+<div class="card">
+        <h3 class="titulo">{{titulo.toLocaleUpperCase()}}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
+                <p>{{ experencia.parrafo }}</p>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 1.5rem;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+</style>
